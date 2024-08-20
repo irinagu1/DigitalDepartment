d4ef4a9ac8f1689c3972edfd2807e3151cb0896a
irinalaptop <noem@mail.ru>
2024-08-20 12:15:07 +0300

    Handle not found exception

---
+++ Entities/Exceptions/DocumentStatusNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public sealed class DocumentStatusNotFoundException : NotFoundException
+    {
+        public DocumentStatusNotFoundException(int id)
+            : base($"The documents status with id: {id} doesnt exist in the database")
+        { }
+    }
+}
