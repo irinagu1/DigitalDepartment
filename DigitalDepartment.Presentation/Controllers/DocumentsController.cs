ea605fbd66b8148cbae1f593e20f42bb7cce91d2
irinalaptop <noem@mail.ru>
2024-08-29 14:08:51 +0300

    Doc creating imlementation

--- DigitalDepartment.Presentation/Controllers/DocumentsController.cs
+++ DigitalDepartment.Presentation/Controllers/DocumentsController.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects.DocumentCategories;
 using Shared.DataTransferObjects.Documents;
+using Shared.RequestFeatures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DigitalDepartment.Presentation.Controllers
@@ -23,20 +25,32 @@ namespace DigitalDepartment.Presentation.Controllers
             _service = service;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetDocuments(
+            [FromQuery] DocumentParameters documentParameters)
+        {
+            var pagedResult = await
+                _service.DocumentService.GetAllDocumentsAsync(
+                    documentParameters, trackChanges: false);
+            Response.Headers.Add("X-Pagination",
+                JsonSerializer.Serialize(pagedResult.metaData));
+            return Ok(pagedResult.documents);
+        }
+
         //create
         [HttpPost(Name = "CreateDocument")]
         [ServiceFilter(typeof(ValidationFilterAttribute))]
         public async Task<IActionResult> CreateDocument([FromForm] DocumentForCreationDto document)
         {
-            //проверка что существует такой статус, такая категория, такое письмо, такой отправитель и что все ок в принципе
-
-            //идем на уровень сервиса и можем вернуть ссылку
             var createdDocument = await _service.DocumentService.CreateDocumentAsync(document);
+            return CreatedAtRoute("DocumentById", new { id = createdDocument.Id, createdDocument });
+        }
 
-            //  var createdDocumentCategory = await _service.DocumentCategoryService.CreateDocumentCategoryAsync(documentCategory);
-            //search this
-            //   return CreatedAtRoute("DocumentCategoryById", new { id = createdDocumentCategory.Id }, createdDocumentCategory);
-            return Ok(createdDocument.Id);
+        [HttpGet("{id:int}", Name = "DocumentById")]
+        public async Task<IActionResult> GetDocument(int id)
+        {
+            var dc = await _service.DocumentService.GetDocumentByIdAsync(id, trackChanges: false);
+            return Ok(dc);
         }
     }
 }
--- Repository/DocsEntities/DocumentRepository.cs
+++ Repository/DocsEntities/DocumentRepository.cs
@@ -2,6 +2,7 @@
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 using Repository.Core;
+using Shared.RequestFeatures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +18,25 @@ namespace Repository.DocsEntities
 
         }
 
+        public async Task<PagedList<Document>> GetAllDocumentsAsync(DocumentParameters documentParameters, bool trackChanges)
+        {
+            var documents = await FindAll(trackChanges)
+                                      .OrderBy(dc => dc.Name)
+                                      .Skip((documentParameters.PageNumber - 1) * documentParameters.PageSize)
+                                      .Take(documentParameters.PageSize)
+                                      .ToListAsync();
+
+            var count = await FindAll(trackChanges).CountAsync();
+            return new PagedList<Document>(documents,
+                                                count,
+                                                documentParameters.PageNumber,
+                                                documentParameters.PageSize);
+        }
+
         public void CreateDocument(Document document) => Create(document);
 
-        public Task<Document> GetDocumentAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<Document> GetDocumentAsync(int id, bool trackChanges) =>
+            FindByCondition(d => d.Id == id, trackChanges).FirstOrDefaultAsync();
 
         public Task<Document> GetDocumentbyPathAsync(string path, bool trackChanges) =>
             FindByCondition(d => d.Path == path, trackChanges).SingleOrDefaultAsync();
